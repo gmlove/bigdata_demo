12abbf105c7078bcda7f02492da178f345c14a5f
gmlove <gracekinglau@gmail.com>
2019-09-25 00:50:10 +0800

    yarn app test

---
+++ test/yarn/src/test/java/parquet/ApplicationMasterTest.java
@@ -0,0 +1,11 @@
+package parquet;
+
+
+import java.util.Arrays;
+
+public class ApplicationMasterTest {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(new String[]{"1", "2"}));
+    }
+}
